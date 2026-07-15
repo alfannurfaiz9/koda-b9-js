cffa0bcb355f24ba0cea22b82741ba4bf6d923e0
alfannurfaiz9 <alfannurfaizwork@gmail.com>
2026-07-14 23:36:00 -0500

    minitask-1

---
+++ mintask-1.js
@@ -0,0 +1,23 @@
+const who = {
+    am: {
+        i: "Alfan Nurfaiz"
+    }
+}
+
+console.log(who.am.i)
+
+const hello = {
+    world: "Hello World"
+}
+console.log(hello.world);
+
+const obj = {
+    str: [,[,,[,,,{koda: [{tech:{academy:"Koda Tech Academy"}}]}]]]
+}
+
+console.log(obj.str[1][2][3].koda[0].tech["academy"]);
+
+const my = [{very: {best: [,{sport:{is: "Football"}}]}}]
+
+console.log(my[0].very["best"][1].sport.is);
+
