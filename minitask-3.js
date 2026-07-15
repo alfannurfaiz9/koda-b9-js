bf45b7f610bdcf0a0adcf18c32e87d08d2af4187
alfannurfaiz9 <alfannurfaizwork@gmail.com>
2026-07-15 06:26:18 -0500

    minitask-3

--- minitask-3.js
+++ minitask-3.js
@@ -1,36 +1,36 @@
-const pembayaran = 150000;
-const totalBelanja = 75000;
-let kembalian = pembayaran - totalBelanja;
+const payment = 150000;
+const totalSpend = 100000;
+let change = payment - totalSpend;
 
-let limaPuluhRibu = 0;
-let sepuluhRibu = 0;
-let limaRibu = 0;
-let duaRibu = 0;
-let seribu = 0;
+let fiftyThousand = 0;
+let tenThousand = 0;
+let fiveThousand = 0;
+let twoThousand = 0;
+let oneThousand = 0;
 
-while (kembalian > 0) {
-  if (kembalian >= 50000) {
-    limaPuluhRibu += 1;
-    kembalian -= 50000;
-  } else if (kembalian >= 10000) {
-    sepuluhRibu += 1;
-    kembalian -= 10000;
-  } else if (kembalian >= 5000) {
-    limaRibu += 1;
-    kembalian -= 5000;
-  } else if (kembalian >= 2000) {
-    duaRibu += 1;
-    kembalian -= 2000;
+while (change > 0) {
+  if (change >= 50000) {
+    fiftyThousand += 1;
+    change -= 50000;
+  } else if (change >= 10000) {
+    tenThousand += 1;
+    change -= 10000;
+  } else if (change >= 5000) {
+    fiveThousand += 1;
+    change -= 5000;
+  } else if (change >= 2000) {
+    twoThousand += 1;
+    change -= 2000;
   } else {
-    seribu += 1;
-    kembalian -= 1000;
+    oneThousand += 1;
+    change -= 1000;
   }
 }
 
 console.log(
-  "Rp50.000: " + limaPuluhRibu + " lembar",
-  "\nRp10.000: " + sepuluhRibu + " lembar",
-  "\nRp5.000: " + limaRibu + " lembar",
-  "\nRp2.000: " + duaRibu + " lembar",
-  "\nRp1.000: " + seribu + " lembar",
+  "Rp50.000: " + fiftyThousand + " lembar",
+  "\nRp10.000: " + tenThousand + " lembar",
+  "\nRp5.000: " + fiveThousand + " lembar",
+  "\nRp2.000: " + twoThousand + " lembar",
+  "\nRp1.000: " + oneThousand + " lembar",
 );
