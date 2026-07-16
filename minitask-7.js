365e88c1a59007b588152f85f6e8824b1588b735
alfannurfaiz9 <alfannurfaizwork@gmail.com>
2026-07-16 14:03:27 +0700

    minitask-7

---
+++ minitask-7.js
@@ -0,0 +1,20 @@
+const scores = {
+  matematika: 80,
+  bahasa_inggris: 60,
+  bahasa_indonesia: 70,
+  ipa: 90,
+};
+
+const sumAndAverageScores = ({
+  matematika,
+  bahasa_inggris,
+  bahasa_indonesia,
+  ipa,
+}) => {
+  const total = matematika + bahasa_inggris + bahasa_indonesia + ipa;
+  const average = total / 4;
+
+  return `Total nilai adalah: ${total}, Rata rata nilai adalah: ${average}`;
+};
+
+console.log(sumAndAverageScores(scores));
