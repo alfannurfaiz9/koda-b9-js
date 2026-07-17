c006aee82af81792cb4301bfe34bfe007e8e988a
alfannurfaiz9 <alfannurfaizwork@gmail.com>
2026-07-17 13:26:00 +0700

    minitask-10

---
+++ minitask-10.js
@@ -0,0 +1,21 @@
+const todos = ["Makan", "Minum", "Mandi", "Olahraga"];
+const input = "Minum";
+
+const customFilter = (arrs) => {
+  let result = [];
+
+  for (let i = 0; i < arrs.length; i++) {
+    if (arrs[i] !== input) {
+      result[result.length] = arrs[i];
+    }
+  }
+
+  return result;
+};
+
+const filteredTodos = todos.filter((todo) => todo !== input);
+const testCustomFilter = customFilter(todos);
+
+console.log(filteredTodos);
+console.log(testCustomFilter);
+console.log(todos);
